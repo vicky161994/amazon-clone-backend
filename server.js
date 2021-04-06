d4539a213d7559bd2177180f8e9ce7fed825345a
Vicky <kumar.v005@gmail.com>
2021-04-06 13:23:44 +0530

    changes for dashboard

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
 import express from "express";
-import data from "./data.js";
+import mongoose from "mongoose";
+import productRouter from "./routers/productRouter.js";
+import userRouter from "./routers/userRouter.js";
+import dotenv from 'dotenv';
+dotenv.config();
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/amazon', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+});
+
+app.use('/api/users', userRouter);
+app.use('/api/products', productRouter);
 app.get('/', (req, res) => res.send('Server is ready!'));
-app.get('/api/products', (req, res) => {
-    res.send(data.products);
+
+app.use((error, req, res, next) => {
+    res.status(500).send({ message: error.message });
 })
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`serve at http://localhost: ${PORT}`));
+app.listen(PORT, () => console.log(`serve at http://localhost:${PORT}`));
