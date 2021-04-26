651efa390c60df0fa0d71d04309497cf243a80a2
Vicky <kumar.v005@gmail.com>
2021-04-26 09:22:38 +0530

    place order implementation

--- server.js
+++ server.js
@@ -2,26 +2,28 @@ import express from "express";
 import mongoose from "mongoose";
 import productRouter from "./routers/productRouter.js";
 import userRouter from "./routers/userRouter.js";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
+import orderRouter from "./routers/orderRouter.js";
 dotenv.config();
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/amazon', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true
+mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/amazon", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
 });
 
-app.use('/api/users', userRouter);
-app.use('/api/products', productRouter);
-app.get('/', (req, res) => res.send('Server is ready!'));
+app.use("/api/users", userRouter);
+app.use("/api/products", productRouter);
+app.use("/api/orders", orderRouter);
+app.get("/", (req, res) => res.send("Server is ready!"));
 
 app.use((error, req, res, next) => {
-    res.status(500).send({ message: error.message });
-})
+  res.status(500).send({ message: error.message });
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`serve at http://localhost:${PORT}`));
--- utils.js
+++ utils.js
@@ -1,11 +1,35 @@
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 export const generateToken = (user) => {
-    return jwt.sign({
-        _id: user._id,
-        name: user.name,
-        email: user.email,
-        isAdmin: user.isAdmin,
-    }, process.env.JWT_SECRET || 'supersecret', {
-        expiresIn: '30d',
-    });
-}
+  return jwt.sign(
+    {
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    },
+    process.env.JWT_SECRET || "supersecret",
+    {
+      expiresIn: "30d",
+    }
+  );
+};
+
+export const isAuth = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (authorization) {
+    const token = authorization.slice(7, authorization.length);
+    jwt.verify(
+      token,
+      process.env.JWT_SECRET || "supersecret",
+      (err, decode) => {
+        if (err) {
+          return res.status(401).send({ message: "Invalid token" });
+        }
+        req.user = decode;
+        next();
+      }
+    );
+  } else {
+    return res.status(401).send({ message: "No Token" });
+  }
+};
