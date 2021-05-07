5da1e99c3cb83d2c36d089520ed4cafd20fe1cec
Vicky <kumar.v005@gmail.com>
2021-05-07 13:33:29 +0530

    db dump added

--- routers/productRouter.js
+++ routers/productRouter.js
@@ -1,26 +1,35 @@
-import express from 'express';
-import expressAsyncHandler from 'express-async-handler';
-import data from '../data.js';
-import Product from '../models/productModel.js';
+import express from "express";
+import expressAsyncHandler from "express-async-handler";
+import data from "../data.js";
+import Product from "../models/productModel.js";
 const productRouter = express.Router();
 
-productRouter.get('/', expressAsyncHandler(async (req, res) => {
+productRouter.get(
+  "/",
+  expressAsyncHandler(async (req, res) => {
     const products = await Product.find({});
     res.send(products);
-}));
+  })
+);
 
-productRouter.get('/seed', expressAsyncHandler(async (req, res) => {
+productRouter.get(
+  "/seed",
+  expressAsyncHandler(async (req, res) => {
     const createdProducts = await Product.insertMany(data.products);
     res.send({ createdProducts });
-}));
+  })
+);
 
-productRouter.get('/:id', expressAsyncHandler(async (req, res) => {
+productRouter.get(
+  "/:id",
+  expressAsyncHandler(async (req, res) => {
     const product = await Product.findById(req.params.id);
     if (product) {
-        res.send(product);
+      res.send(product);
     } else {
-        res.status(400).send({ message: 'Product not Found' });
+      res.status(400).send({ message: "Product not Found" });
     }
-}));
+  })
+);
 
 export default productRouter;
